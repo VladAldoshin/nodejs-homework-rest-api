079d54fd8dfba451ce926d2836fe9e5d409ddb3d
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-09-26 12:19:27 +0300

    add files

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require("mongoose");
+const {handleMongooseError} = require("../helpers")
+const Joi = require('joi');
+
+const emailRegexp = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
+
+const userSchema = new Schema (
+    {
+        password: {
+          type: String,
+          minlength: 6,
+          required: [true, 'Password is required'],
+        },
+        email: {
+          type: String,
+          match: emailRegexp,
+          required: [true, 'Email is required'],
+          unique: true,
+        },
+        subscription: {
+          type: String,
+          enum: ["starter", "pro", "business"],
+          default: "starter"
+        },
+        token: {
+          type: String,
+          default: null,
+        },
+        avatarURL: {
+          type: String,
+          required: true,
+        },
+      }, { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save",handleMongooseError)
+
+const registerSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+  });
+  
+  const loginSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+  });
+  
+ 
+  
+  const User = model("user", userSchema);
+
+  const schemas = {
+    registerSchema,
+    loginSchema,
+  }
+  
+  module.exports = {
+    User,
+    schemas,
+    };
