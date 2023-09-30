58b3efb7a29cfc17a849a48faf88fb0c7faba76d
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-09-30 20:27:10 +0300

    fix auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,7 +48,7 @@ const verifyEmail = async (req, res) => {
 
   await User.findByIdAndUpdate(user._id, {
     verify: true,
-    verificationToken: "",
+    verificationToken: null,
   });
 
   res.status(200).json({
